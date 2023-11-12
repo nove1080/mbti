aa751bab9ffe1b7200b857e0ac86e310cf00d78e
nove1080 <skwpqjq@gmail.com>
2023-11-12 23:54:55 +0900

    MBTI-004 feat: 채팅방 컨트롤러에 member_id를 적용

--- src/main/java/com/example/demo/web/controller/v1/ChatRoomController.java
+++ src/main/java/com/example/demo/web/controller/v1/ChatRoomController.java
@@ -4,15 +4,18 @@ import com.example.demo.domain.usecase.chatroom.CreateChatRoomListService;
 import com.example.demo.domain.usecase.chatroom.CreateChatRoomService;
 import com.example.demo.domain.usecase.chatroom.ReadChatRoomService;
 import com.example.demo.domain.usecase.chatroom.ReadChatSurveyQuestionService;
+import com.example.demo.security.authentication.token.TokenUserDetailsService;
 import com.example.demo.support.ApiResponse;
 import com.example.demo.support.ApiResponseGenerator;
 import com.example.demo.web.dto.request.CreateChatRoomRequest;
 import com.example.demo.web.dto.response.ChatRoomResponse;
 import com.example.demo.web.dto.response.ChatSurveyQuestionResponse;
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -23,21 +26,34 @@ public class ChatRoomController {
 
 	private final CreateChatRoomService createChatRoomService;
 	private final ReadChatRoomService readChatRoomService;
+	private final ReadChatSurveyQuestionService readChatSurveyQuestionService;
 	private final CreateChatRoomListService createChatRoomListService;
 
+	private final TokenUserDetailsService tokenUserDetailsService;
+
 	@PostMapping
-	public String save(@RequestBody @Valid CreateChatRoomRequest request) {
-		Long memberId = 1L;
-		Long chatroomId = createChatRoomService.execute(request);
-		createChatRoomListService.create(1L, chatroomId);
+	public String save(
+			@RequestBody @Valid CreateChatRoomRequest requestData, HttpServletRequest request) {
+		Long memberId = findMemberByToken(request);
+		Long chatroomId = createChatRoomService.execute(requestData);
+
+		createChatRoomListService.create(memberId, chatroomId);
 		return "redirect:/api/v1/chatrooms";
 	}
 
 	@GetMapping
-	public ApiResponse<ApiResponse.SuccessBody<ChatRoomResponse>> loadChatRooms() {
-		Long memberId = 1L;
+	public ApiResponse<ApiResponse.SuccessBody<ChatRoomResponse>> loadChatRooms(
+			HttpServletRequest request) {
+		Long memberId = findMemberByToken(request);
 		ChatRoomResponse res = readChatRoomService.execute(memberId);
 		return ApiResponseGenerator.success(res, HttpStatus.OK);
 	}
 
+	private Long findMemberByToken(HttpServletRequest request) {
+		String authentication = request.getHeader("Authentication");
+		String substring = authentication.substring(7, authentication.length());
+		UserDetails userDetails = tokenUserDetailsService.loadUserByUsername(substring);
+		Long memberId = Long.parseLong(userDetails.getUsername());
+		return memberId;
+	}
 }
