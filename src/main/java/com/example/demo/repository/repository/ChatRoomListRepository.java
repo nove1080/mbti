ec721388521976dc2ba31c22c8fec36f18ec06a5
MoonYeongSang <118742845+MoonYeongSang@users.noreply.github.com>
2023-11-18 15:49:18 +0900

    MBTI-007 feat: ChatRoomListRepository findAllByChatRoomId 추가

--- src/main/java/com/example/demo/repository/repository/ChatRoomListRepository.java
+++ src/main/java/com/example/demo/repository/repository/ChatRoomListRepository.java
@@ -9,4 +9,6 @@ import org.springframework.stereotype.Repository;
 public interface ChatRoomListRepository extends JpaRepository<ChatRoomListEntity, Long> {
 
 	List<ChatRoomListEntity> findAllByMemberId(Long memberId);
+
+	List<ChatRoomListEntity> findAllByChatRoomId(Long chatRoomId);
 }
